b1ea09d27cf7d2fc65f4f3367b510f7cd4cc8ab0
tonylex12 <tonylex12@gmail.com>
2021-09-10 09:14:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>LEXTEC - Ecommerce</h1>
+        <h3>By Antony Huerto 2021</h3>
       </header>
     </div>
   );
